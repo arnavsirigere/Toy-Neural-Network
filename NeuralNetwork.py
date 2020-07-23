848e6316da441072981d927b8801252e2a2f3b26
Arnav Sirigere <arnavsirigere@gmail.com>
2020-07-23 17:57:45 +0530

    🎇 Initial Commit

---
+++ NeuralNetwork.py
@@ -0,0 +1,92 @@
+from matrix import Matrix
+import math
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+def dsigmoid(x):
+    return x * (1 - x)
+
+
+class NeuralNetwork:
+    def __init__(self, input_nodes, hidden_nodes, output_nodes):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+
+        self.weights_ih = Matrix(self.hidden_nodes, self.input_nodes)
+        self.weights_ho = Matrix(self.output_nodes, self.hidden_nodes)
+        self.weights_ih.randomize()
+        self.weights_ho.randomize()
+
+        self.weights_ho_t = Matrix.transpose(self.weights_ho)
+
+        self.bias_h = Matrix(self.hidden_nodes, 1)
+        self.bias_o = Matrix(self.output_nodes, 1)
+        self.bias_h.randomize()
+        self.bias_o.randomize()
+
+        self.learning_rate = 0.1
+
+    def predict(self, input_array):
+        # Computing Hidden Outputs
+        inputs = Matrix.fromArray(input_array)
+        hidden = Matrix.multiply1(self.weights_ih, inputs)
+        hidden.add(self.bias_h)
+        hidden.map1(sigmoid)  # Activation Function
+
+        # Computing Output Layer's Output!
+        outputs = Matrix.multiply1(self.weights_ho, hidden)
+        outputs.add(self.bias_o)
+        outputs.map1(sigmoid)
+
+        return outputs.toArray()
+
+    def train(self, input_array, target_array):
+        # Computing Hidden Outputs
+        inputs = Matrix.fromArray(input_array)
+        hidden = Matrix.multiply1(self.weights_ih, inputs)
+        hidden.add(self.bias_h)
+        hidden.map1(sigmoid)  # Activation function
+
+        # Computing Output Layer's Output!
+        outputs = Matrix.multiply1(self.weights_ho, hidden)
+        outputs.add(self.bias_o)
+        outputs.map1(sigmoid)  # Neural Net's Guess
+
+        # Converting target array to matrix object
+        targets = Matrix.fromArray(target_array)
+
+        # Calculate Error
+        output_errors = Matrix.subtract(targets, outputs)
+
+        # Calculate Hidden Errors
+        hidden_errors = Matrix.multiply1(self.weights_ho_t, output_errors)
+
+        # Calculate gradients
+        gradients = Matrix.map2(outputs, dsigmoid)
+        gradients.multiply2(output_errors)
+        gradients.multiply2(self.learning_rate)
+
+        # Calculate Deltas
+        hidden_t = Matrix.transpose(hidden)
+        weight_ho_deltas = Matrix.multiply1(gradients, hidden_t)
+
+        # Adjust Hidden -> Output weights and output layer's biases
+        self.weights_ho.add(weight_ho_deltas)
+        self.bias_o.add(gradients)
+
+        # Calculate the hidden gradients
+        hidden_gradients = Matrix.map2(hidden, dsigmoid)
+        hidden_gradients.multiply2(hidden_errors)
+        hidden_gradients.multiply2(self.learning_rate)
+
+        # Calculate Deltas
+        input_t = Matrix.transpose(inputs)
+        weight_ih_deltas = Matrix.multiply1(hidden_gradients, input_t)
+
+        # Adjust Input -> Hidden weights and hidden biases
+        self.weights_ih.add(weight_ih_deltas)
+        self.bias_h.add(hidden_gradients)
---
+++ matrix.py
@@ -0,0 +1,109 @@
+from random import random
+
+
+class Matrix:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.data = []
+
+        for i in range(self.rows):
+            self.data.append([])
+            for _ in range(self.cols):
+                self.data[i].append(0)
+
+    def print(self):
+        print(self.data)
+
+    def fromArray(arr):
+        m = Matrix(len(arr), 1)
+        for i in range(len(arr)):
+            m.data[i][0] = arr[i]
+        return m
+
+    def toArray(self):
+        arr = []
+        for i in range(self.rows):
+            for j in range(self.cols):
+                arr.append(self.data[i][j])
+        return arr
+
+    def randomize(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                r = random()
+                r = r if random() < 0.5 else r - 1
+                self.data[i][j] = r
+
+    def subtract(a, b):
+        result = Matrix(a.rows, a.cols)
+        for i in range(a.rows):
+            for j in range(a.cols):
+                result.data[i][j] = a.data[i][j] - b.data[i][j]
+        return result
+
+    def add(self, n):
+        if isinstance(n, Matrix):
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.data[i][j] += n.data[i][j]
+        else:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.data[i][j] += n
+
+    def transpose(matrix):
+        result = Matrix(matrix.cols, matrix.rows)
+        for i in range(matrix.rows):
+            for j in range(matrix.cols):
+                result.data[j][i] = matrix.data[i][j]
+        return result
+
+    # Matrix Multiplication
+    def multiply1(a, b):
+        if a.cols != b.rows:
+            print('Invalid Matrices!')
+            return
+
+        result = Matrix(a.rows, b.cols)
+
+        for i in range(result.rows):
+            for j in range(result.cols):
+                s = 0.0
+                for k in range(a.cols):
+                    s += a.data[i][k] * b.data[k][j]
+                result.data[i][j] = s
+        return result
+
+    def multiply2(self, n):
+        if isinstance(n, Matrix):
+            if self.rows != n.rows or self.cols != n.cols:
+                print('Invalid Matrices!')
+                return
+            # Hadamard Product
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.data[i][j] *= n.data[i][j]
+        else:
+            # Scalar Product
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.data[i][j] *= n
+
+    def map1(self, func):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.data[i][j] = func(self.data[i][j])
+
+    def map2(m, func):
+        result = Matrix(m.rows, m.cols)
+        for i in range(result.rows):
+            for j in range(result.cols):
+                result.data[i][j] = func(m.data[i][j])
+        return result
+
+
+Matrix.fromArray = staticmethod(Matrix.fromArray)
+Matrix.transpose = staticmethod(Matrix.transpose)
+Matrix.multiply1 = staticmethod(Matrix.multiply1)
+Matrix.map2 = staticmethod(Matrix.map2)
